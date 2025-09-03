715dc472e2d87ffb67bea29a97eb7e671598bffc
rfgIII <rfg17@pitt.edu>
2025-09-03 14:19:05 -0400

    Added plic_rocket_warm_irqchip_init method. hopefully this fixes fw_jump.bin

--- platform/litex/rocket/platform.c
+++ platform/litex/rocket/platform.c
@@ -90,6 +90,28 @@ static int ROCKET_console_init(void)
 	return litex_uart_init(ROCKET_DEFAULT_UART_ADDR);
 }
 
+static int plic_rocket_warm_irqchip_init(int m_cntx_id, int s_cntx_id)
+{
+	int ret;
+
+	/* By default, enable all IRQs for M-mode of target HART */
+	if (m_cntx_id > -1) {
+		ret = plic_context_init(&plic, m_cntx_id, true, 0x1);
+		if (ret)
+			return ret;
+	}
+
+	/* Enable all IRQs for S-mode of target HART */
+	if (s_cntx_id > -1) {
+		ret = plic_context_init(&plic, s_cntx_id, true, 0x0);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
+
 /*
  * Initialize the ROCKETRiscv interrupt controller for current HART.
  */
@@ -104,7 +126,7 @@ static int ROCKET_irqchip_init(bool cold_boot)
 			return rc;
 	}
 
-	return plic_warm_irqchip_init(&plic, hartid * 2, hartid * 2 + 1);
+	return plic_rocket_warm_irqchip_init(&plic, hartid * 2, hartid * 2 + 1);
 
 }
 
